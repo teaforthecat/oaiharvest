aa4583489b4581f591b97bceb1809c41d0d3bb56
Chris Thompson <chris.thompson@walkerart.org>
2012-04-30 12:50:31 -0500

    verb tests

--- spec/lib/oaiharvest/client_spec.rb
+++ spec/lib/oaiharvest/client_spec.rb
@@ -1,6 +1,4 @@
 require_relative '../../spec_helper'
-# For Ruby < 1.9.3, use this instead of require_relative
-# require (File.expand_path('./../../../spec_helper', __FILE__))
 
 describe Oaiharvest::Client do
   
@@ -8,26 +6,62 @@ describe Oaiharvest::Client do
     Oaiharvest::Client.must_include HTTParty
   end
   
-  it "must have the base url set to an oai endpoint" do
-    Oaiharvest::Client.base_uri.must_equal 'http://collections.walkerart.org/oai'
+  it "starts with empty base url" do
+    Oaiharvest::Client.base_uri.must_be_nil
   end 
 
-  describe "GET verb=Identify" do
-    let(:client) { Oaiharvest::Client.new }
+  describe "verbs" do
     
     before do
-    VCR.insert_cassette 'client', :record => :new_episodes
+      @options = {:base_uri => 'collections.walkerart.org:8083/oai'}
+      VCR.insert_cassette 'client', :record => :new_episodes
     end
     
     after do
       VCR.eject_cassette
     end
+    let(:client) { Oaiharvest::Client.new @options }
 
-    it "has a harvest method" do 
-      client.must_respond_to :harvest
+    it "requires metadata_prefix " do 
+      lambda{client.list_records}.must_raise(RuntimeError,"metadata_prefix required")        
     end
 
-    it "it identifies a target" do
+    it "forms a valid query" do
+      client.make_query.must_equal({})
+      client.make_query({:metadata_prefix=>"oai_dc"}).must_equal({'metadataPrefix' => "oai_dc"})
+    end
+
+    it "ListRecords: returns records with headers and metadata" do 
+      listed_records = client.list_records( {:metadata_prefix => "oai_dc"} )
+      listed_records[0].must_respond_to :header
+      listed_records[0].must_respond_to :metadata
+      listed_records[0].header.must_include("identifier")
+      listed_records[0].metadata.must_include("identifier")
+    end
+
+    it "ListMetadataFormats: returns list of formats" do 
+      listed_formats = client.list_metadata_formats
+      listed_formats.must_be_instance_of Array
+      listed_formats[0].must_be_instance_of Hash
+      listed_formats[0].must_include "metadata_prefix"
+      listed_formats[0]["metadata_prefix"].must_equal "oai_dc"
+    end
+
+    it "ListIdentifiers: returns headers " do 
+      listed_identifiers = client.list_identifiers
+      listed_identifiers.must_be_instance_of Array
+      listed_identifiers[0].must_be_instance_of Hash
+      listed_identifiers[0].must_include(  "identifier" )
+      listed_identifiers[0]["identifier"].must_equal "oai:walkerart.org/object/1"
+    end
+
+    it "ListSets: returns an Array of sets " do 
+      listed_sets = client.list_sets
+      listed_sets.must_be_instance_of Array
+      listed_sets.must_equal ["object", "text", "sound", "video", "sia", "artfinder"]
+    end
+
+    it "Identify: it identifies a target" do
       identified = client.identify
       identified.must_be_instance_of Hash
       identified[:repository_name].must_equal "Walker Art Center Collection Metadata"
@@ -36,8 +70,37 @@ describe Oaiharvest::Client do
       identified[:earliest_datestamp].must_equal "2002-02-15"
       identified[:deleted_record].must_equal "persistent"
       identified[:granularity].must_equal "YYYY-MM-DD"
-      
     end
+  end
+
+
+
+  describe "Client options" do 
+    before do 
+      options = {:base_uri => 'collections.walkerart.org:8083/oai'}
+      @set_options = options.merge({:set => 'objects'})
+    end
+
+    let(:client) { Oaiharvest::Client.new @set_options}
+    
+    it "sets set" do
+      client.opts[:set].must_equal 'objects'
+    end
+
+    it "base_uri setter is private" do 
+      client.wont_respond_to :base_uri=
+      client.class.wont_respond_to :base_uri=
+      client.class.base_uri.must_equal 'http://collections.walkerart.org:8083/oai'
+    end
+
+    it "can change base_uri on the fly" do 
+      client.class.must_respond_to :base_uri
+      old_uri = client.class.base_uri
+      client.class.base_uri 'test'
+      client.class.base_uri.must_equal 'http://test'
+      client.class.base_uri old_uri      
+    end
+
   end
 
 end
